ed7bd5151a0a1c6197aa114d547fa9fe6cce4850
Ehedei Hernandez <ehernandez@eturia.es>
2022-12-27 09:55:31 +0000

    Move main to package

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
+package main;
+
 /**
  * @author {@link "mailto:ehernandez@eturia.es"}
- */public class Main {
+ */
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
